ffc3377e17e6b4c0f9329ad933162706c4bcae9f
fsfalcao <fsfalcao@AGIN110>
2021-04-26 12:51:11 -0300

    integracao

---
+++ clinica_medica/lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+bool USE_FIRESTORE_EMULATOR = false;
+
+Future<void> main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  // if (USE_FIRESTORE_EMULATOR) {
+  //   FirebaseFirestore.instance.settings = const Settings(
+  //       host: 'localhost:8080', sslEnabled: false, persistenceEnabled: false);
+  // }
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Integração Firebase',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Integração Firebase'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  // void _incrementCounter() {
+  //   setState(() {
+  //     _counter++;
+  //   });
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: StreamBuilder(
+        stream:
+            FirebaseFirestore.instance.collection('funcionario').snapshots(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          final documents = snapshot.data;
+          return ListView.builder(
+            itemCount: documents.size,
+            itemBuilder: (ctx, i) => Container(
+              padding: EdgeInsets.all(10),
+              child: Text(documents.docs[i]['nome']),
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          FirebaseFirestore.instance.collection('funcionario').add({
+            'nome': 'TesteFlutter2',
+            'email': 'testeflutter@teste',
+            'carteiraTrabalho': 'teste',
+            'telefone': '9999999',
+            'dataContratacao': DateTime.now()
+          });
+          // .snapshots()
+          //     .listen((querySnapshot) {
+          //   // print(querySnapshot.docs[0]['nome']);
+          //   querySnapshot.docs.forEach((element) {
+          //     print(element['nome']);
+          //   });
+          // });
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
