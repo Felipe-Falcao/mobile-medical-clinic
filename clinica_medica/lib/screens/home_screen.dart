b0732e68f11ef6f82cb944d181ad485ee2de913f
Alanfxx <j.alands2@gmail.com>
2021-07-11 10:05:08 -0300

    ajuste layout homescreen

--- clinica_medica/lib/screens/home_screen.dart
+++ clinica_medica/lib/screens/home_screen.dart
@@ -15,6 +15,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final AuthenticationFB auth = new AuthenticationFB();
   @override
   void initState() {
     Patients patients = Provider.of<Patients>(context, listen: false);
@@ -38,116 +39,142 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final AuthenticationFB auth = new AuthenticationFB();
     UserProvider userProv = Provider.of<UserProvider>(context);
     bool isAdmin = userProv.isAdmin;
     bool isAttendant = userProv.isAttendant;
     bool isDoctor = userProv.isDoctor;
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-        actions: [
-          DropdownButton(
-            icon: Icon(
-              Icons.more_vert,
-              color: Theme.of(context).primaryIconTheme.color,
-            ),
-            items: [
-              DropdownMenuItem(
-                value: 'logout',
-                child: Container(
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.exit_to_app,
-                        color: Colors.black,
-                      ),
-                      SizedBox(width: 8),
-                      Text('Sair'),
-                    ],
+      body: Stack(
+        children: [
+          Container(
+            width: size.width,
+            height: size.height * .27,
+            color: Theme.of(context).primaryColor,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Row(
+                children: [
+                  Text(
+                    'Clinic+',
+                    style: TextStyle(fontFamily: 'Iceberg', fontSize: 40),
                   ),
+                  Spacer(),
+                  _button(),
+                ],
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              SizedBox(height: size.height * .18),
+              Container(
+                height: size.height * .72,
+                child: GridView.count(
+                  primary: false,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+                  crossAxisSpacing: 18,
+                  mainAxisSpacing: 20,
+                  crossAxisCount: 3,
+                  childAspectRatio: 9 / 10,
+                  children: <Widget>[
+                    _item(
+                      context: context,
+                      icons: Icons.attribution_rounded,
+                      label: 'Gerenciar Atendente',
+                      nav: () => Navigator.of(context)
+                          .pushReplacementNamed(AppRoutes.ATTENDANT_SCREEN),
+                      hasAccess: isAdmin,
+                    ),
+                    _item(
+                      context: context,
+                      icons: Icons.people,
+                      label: 'Gerenciar Médicos',
+                      nav: () => Navigator.of(context)
+                          .pushReplacementNamed(AppRoutes.DOCTOR_SCREEN),
+                      hasAccess: isAdmin,
+                    ),
+                    _item(
+                      context: context,
+                      icons: Icons.people_alt_rounded,
+                      label: 'Gerenciar Pacientes',
+                      nav: () => Navigator.of(context)
+                          .pushReplacementNamed(AppRoutes.PATIENT_SCREEN),
+                      hasAccess: isAdmin || isAttendant || isDoctor,
+                    ),
+                    _item(
+                      context: context,
+                      icons: Icons.event_note_rounded,
+                      label: 'Gerenciar Agendamentos',
+                      nav: () => Navigator.of(context)
+                          .pushReplacementNamed(AppRoutes.SCHEDULE_SCREEN),
+                      hasAccess: isAdmin || isAttendant || isDoctor,
+                    ),
+                    _item(
+                      context: context,
+                      icons: Icons.event_available_rounded,
+                      label: 'Gerenciar Consulta',
+                      nav: () => Navigator.of(context)
+                          .pushReplacementNamed(AppRoutes.APPOINTMENT_SCREEN),
+                      hasAccess: isAdmin || isAttendant || isDoctor,
+                    ),
+                    _item(
+                      context: context,
+                      icons: Icons.medical_services,
+                      label: 'Gerenciar Medicamentos',
+                      nav: null,
+                      //TODO - aterar quando ger. medicamentos estiver pronto
+                      hasAccess: false,
+                      // hasAccess: isAdmin || isDoctor,
+                    ),
+                    _item(
+                      context: context,
+                      icons: Icons.note_alt_rounded,
+                      label: 'Gerenciar Prontuário',
+                      nav: () => Navigator.of(context)
+                          .pushReplacementNamed(AppRoutes.CHART_SCREEN),
+                      hasAccess: isAdmin || isAttendant || isDoctor,
+                    ),
+                  ],
                 ),
-              )
+              ),
             ],
-            onChanged: (item) {
-              if (item == 'logout') {
-                auth.signOut();
-              }
-            },
-          )
-        ],
-      ),
-      // appBar: AppBar(title: const Text('Home')
-      // ),
-      body: GridView.count(
-        primary: false,
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
-        crossAxisSpacing: 18,
-        mainAxisSpacing: 20,
-        crossAxisCount: 3,
-        childAspectRatio: 9 / 10,
-        children: <Widget>[
-          _item(
-            context: context,
-            icons: Icons.attribution_rounded,
-            label: 'Gerenciar Atendente',
-            nav: () => Navigator.of(context)
-                .pushReplacementNamed(AppRoutes.ATTENDANT_SCREEN),
-            hasAccess: isAdmin,
-          ),
-          _item(
-            context: context,
-            icons: Icons.people,
-            label: 'Gerenciar Médicos',
-            nav: () => Navigator.of(context)
-                .pushReplacementNamed(AppRoutes.DOCTOR_SCREEN),
-            hasAccess: isAdmin,
-          ),
-          _item(
-            context: context,
-            icons: Icons.people_alt_rounded,
-            label: 'Gerenciar Pacientes',
-            nav: () => Navigator.of(context)
-                .pushReplacementNamed(AppRoutes.PATIENT_SCREEN),
-            hasAccess: isAdmin || isAttendant || isDoctor,
-          ),
-          _item(
-            context: context,
-            icons: Icons.event_note_rounded,
-            label: 'Gerenciar Agendamentos',
-            nav: () => Navigator.of(context)
-                .pushReplacementNamed(AppRoutes.SCHEDULE_SCREEN),
-            hasAccess: isAdmin || isAttendant || isDoctor,
-          ),
-          _item(
-            context: context,
-            icons: Icons.event_available_rounded,
-            label: 'Gerenciar Consulta',
-            nav: () => Navigator.of(context)
-                .pushReplacementNamed(AppRoutes.APPOINTMENT_SCREEN),
-            hasAccess: isAdmin || isAttendant || isDoctor,
-          ),
-          _item(
-            context: context,
-            icons: Icons.medical_services,
-            label: 'Gerenciar Medicamentos',
-            nav: null,
-            //TODO - aterar quando ger. medicamentos estiver pronto
-            hasAccess: false,
-            // hasAccess: isAdmin || isDoctor,
-          ),
-          _item(
-            context: context,
-            icons: Icons.note_alt_rounded,
-            label: 'Gerenciar Prontuário',
-            nav: () => Navigator.of(context)
-                .pushReplacementNamed(AppRoutes.CHART_SCREEN),
-            hasAccess: isAdmin || isAttendant || isDoctor,
           ),
         ],
       ),
     );
   }
+
+  _button() => DropdownButton(
+        underline: SizedBox(),
+        icon: Icon(
+          Icons.more_vert,
+          color: Theme.of(context).primaryIconTheme.color,
+        ),
+        items: [
+          DropdownMenuItem(
+            value: 'logout',
+            child: Container(
+              child: Row(
+                children: [
+                  Icon(
+                    Icons.exit_to_app,
+                    color: Colors.black,
+                  ),
+                  SizedBox(width: 8),
+                  Text('Sair'),
+                ],
+              ),
+            ),
+          )
+        ],
+        onChanged: (item) {
+          if (item == 'logout') {
+            auth.signOut();
+          }
+        },
+      );
 
   Widget _item({
     @required BuildContext context,
