c7af47e455cfbbeb4e454e22714c336aa88cc7e1
Felipe-Falcao <felipe.falcao@dcomp.ufs.br>
2021-07-12 22:49:01 -0300

    Comentarios controller

--- clinica_medica/lib/controllers/endereco_controller.dart
+++ clinica_medica/lib/controllers/endereco_controller.dart
@@ -1,8 +1,16 @@
 import 'package:clinica_medica/infra/endereco_connect.dart';
 
+/**
+ * Classe responsável por realizar o controle das requisições de Endereço da 
+ * camada de Infra.
+ */
 class EnderecoController {
   EnderecoFB enderecoFB = new EnderecoFB();
 
+  /**
+   * Função responsável por receber dados de endereço e chamar a função
+   * de atualização de endereco.
+   */
   Future<void> atualizarEndereco(infoEndereco) async {
     try {
       await enderecoFB.update(
@@ -17,6 +25,10 @@ class EnderecoController {
     }
   }
 
+  /**
+   * Função responsável por receber um id de endereço e  chamar a função 
+   * de busca de endereço. Retorna um Map de strings com os valores obtidos.
+   */
   Future<Map<String, dynamic>> buscarEndereco(enderecoId) async {
     try {
       return await enderecoFB.read(enderecoId);
